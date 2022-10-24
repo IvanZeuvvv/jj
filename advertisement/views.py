e17caaea9579036de77687beb63c31703d3c5977
Иван Зуев <ivanzuevvv@gmial.com>
2022-10-24 18:20:40 +0500

    My

--- advertisement/views.py
+++ advertisement/views.py
@@ -14,3 +14,10 @@ class AdvertisementDetailView(generic.DetailView):
     model = Advertisement
     template_name = 'advertisement/advertisement-detail.html'
     context_object_name = 'detail_list'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.views += 1
+        self.object.save()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
